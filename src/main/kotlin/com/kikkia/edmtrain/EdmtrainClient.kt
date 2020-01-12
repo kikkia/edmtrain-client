f7195d25eac8529caeb7d23cd41917713ebdfb03
JessWalters <jessw@iastate.edu>
2020-01-11 19:12:46 -0600

    add some docs

--- src/main/kotlin/com/kikkia/edmtrain/EdmtrainClient.kt
+++ src/main/kotlin/com/kikkia/edmtrain/EdmtrainClient.kt
@@ -54,59 +54,94 @@ class EdmtrainClient(builder: Builder) {
             formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
         }
 
+        /**
+         * Limit the query with a given event name
+         */
         fun withEventName(name: String): EventQuery {
             this.args["eventName"] = name
             return this
         }
 
+        /**
+         * Limit the query with a given list of artist Ids
+         */
         fun withArtistIds(artistIds: List<Int>): EventQuery {
             val idStrings = artistIds.map { it.toString() }
             this.args["artistIds"] = idStrings.joinToString(",")
             return this
         }
 
+        /**
+         * Limit the query with a given list of venue Ids
+         */
         fun withVenueIds(venueIds: List<Int>): EventQuery {
             val idStrings = venueIds.map { it.toString() }
             this.args["venueIds"] = idStrings.joinToString(",")
             return this
         }
 
+        /**
+         * Limit the query with a list of location Ids
+         */
         fun withLocationIds(locationIds: List<Int>): EventQuery {
             val idStrings = locationIds.map { it.toString() }
             this.args["locationIds"] = idStrings.joinToString(",")
             return this
         }
 
+        /**
+         * Limit the query to events occurring at or after this local date (only future events will be returned).
+         */
         fun withStartDate(startDate: LocalDate): EventQuery {
             this.args["startDate"] = startDate.format(formatter)
             return this
         }
 
+        /**
+         * Limit the query to events occurring at or before this local date.
+         */
         fun withEndDate(endDate: LocalDate): EventQuery {
             this.args["endDate"] = endDate.format(formatter)
             return this
         }
 
+        /**
+         * Limit the query to events added to Edmtrain at or after this UTC date (only future events will be returned).
+         */
         fun withCreatedStartDate(createdStartDate: LocalDate): EventQuery {
             this.args["createdStartDate"] = createdStartDate.format(formatter)
             return this
         }
 
+        /**
+         * Limit the query to events added to Edmtrain at or before this UTC date (only future events will be returned).
+         */
         fun withCreatedEndDate(createdEndDate: LocalDate): EventQuery {
             this.args["createdEndDate"] = createdEndDate.format(formatter)
             return this
         }
 
+        /**
+         * Limit the query to events that are festivals (true) or not festivals (false) (default: both)
+         */
         fun isFestival(isFestival: Boolean): EventQuery {
             this.args["festivalInd"] = isFestival.toString()
             return this
         }
 
+
+        /**
+         * Set to true to include electronic shows (default is true). Set to false to exclude electronic shows.
+         */
         fun includeElectronicGenre(includeElectronicGenre: Boolean): EventQuery {
             this.args["includeElectronicGenreInd"] = includeElectronicGenre.toString()
             return this
         }
 
+
+        /**
+         * Set to true to include non-electronic shows (default is false).
+         */
         fun includeOtherGenre(includeOtherGenre: Boolean): EventQuery {
             this.args["includeOtherGenreInd"] = includeOtherGenre.toString()
             return this
@@ -148,11 +183,17 @@ class EdmtrainClient(builder: Builder) {
             args = HashMap()
         }
 
+        /**
+         *  (optional unless city is supplied) – The state or province of the desired location.
+         */
         fun withState(state: String): LocationQuery {
             args["state"] = state
             return this
         }
 
+        /**
+         *  (optional) – The city or town of the desired location.
+         */
         fun withCity(city: String): LocationQuery {
             args["city"] = city
             return this
